550280c7e9aeb0ce591ef30ae0c836c75b2e0dd7
leooo <leonardo2006santosgonca@gmail.com>
2025-03-28 11:33:39 -0300

    Parate100

---
+++ Parate100/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NumeroParAte100
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+
+            int contador;
+
+
+            for (contador = 2; contador <= 100; contador++) 
+            {
+                if (contador % 2 == 0)
+                {
+                    Console.WriteLine(contador + "\n");
+                }
+
+            }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
