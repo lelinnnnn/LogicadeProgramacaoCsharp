261eff77fbea49067f6570c4c9e2e6392ec3557d
leooo <leonardo2006santosgonca@gmail.com>
2025-03-28 11:06:08 -0300

    28/03

---
+++ Calculardesconto/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculardesconto
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            double preco, desconto;
+
+
+            Console.WriteLine("Digite o preço do produto: ");
+            preco = double.Parse(Console.ReadLine());
+
+
+            if (preco > 100)
+            {
+                desconto = preco * 0.10;
+                Console.WriteLine(desconto + " reais de desconto");
+            }
+            else
+            {
+                {
+                    desconto = preco * 0.05;
+                    Console.WriteLine(desconto + " reais de desconto");
+
+                }
+
+
+
+
+
+            }
+        }
+    }
+}
---
+++ PositivoNegativouZero/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PositivoNegativouZero
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int numero;
+                Console.WriteLine("VOU FALAR O QUE É O NÚMERO");
+                Console.WriteLine("Digite um número: ");
+                numero = int.Parse(Console.ReadLine());
+            
+
+            if (numero == 0)
+            {
+                Console.WriteLine("Zero");
+            }
+            else if (numero > 0)
+            {
+                Console.WriteLine("Número positivo");
+            }
+            else
+            {
+                Console.WriteLine("Número negativo");
+            }
+
+        
+        }
+    }
+}
