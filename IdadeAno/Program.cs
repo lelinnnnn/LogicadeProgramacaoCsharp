c08f4c9ab12c8acc44cb93feabf1fa81336090dc
leooo <leonardo2006santosgonca@gmail.com>
2025-04-02 09:51:32 -0300

    IdadeAno

---
+++ IdadeAno/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IdadeAno
+{
+    internal class Program
+    {
+
+        static void Main(string[] args, int contador)
+        {
+            
+                int ano, anonascimento, idade;
+            while (contador > 0)
+            {
+                Console.WriteLine("Digite sua idade");
+                idade = int.Parse(Console.ReadLine());
+                Console.WriteLine("Digite o ano atual");
+                ano = int.Parse(Console.ReadLine());
+
+
+                    anonascimento = (ano - idade);
+
+
+
+
+                if (idade == 0)
+                {
+
+                }
+            }
+
+        }
+    }
+}
